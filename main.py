3f863b4065885b79ac3b37f3352fd500be829107
Roger Godofredo Rivero Morales <rgrivero62@gmail.com>
2022-01-19 21:53:52 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Scientific Computing with Python Certification (300 hours)"""
+from unittest import main
+
+from prob_calculator import Hat, experiment
+
+hat = Hat(blue=4, red=2, green=6)
+probability = experiment(hat=hat, expected_balls={"blue": 2, "red": 1}, num_balls_drawn=4, num_experiments=3000)
+print("Probability:", probability)
+
+main(module='test_module', exit=False)
